83c91dafb871f296c737440c6be682334683c7e9
denis <new79den@gmail.com>
2016-05-11 22:28:30 +0300

    добавлено View

--- autoload.php
+++ autoload.php
@@ -2,10 +2,12 @@
 function __autoload($class)
 {
     if (file_exists(__DIR__ . '/controllers/' . $class . ".php")) {
-        require __DIR__ . '/controllers/' . $class . ".php";
+        require_once __DIR__ . '/controllers/' . $class . ".php";
     } elseif (file_exists(__DIR__ . '/models/' . $class . ".php")) {
-        require __DIR__ . '/models/' . $class . ".php";
+        require_once __DIR__ . '/models/' . $class . ".php";
     } elseif (file_exists(__DIR__ . '/classes/' . $class . ".php")) {
-        require __DIR__ . 'classes/' . $class . ".php";
+        require_once __DIR__ . '/classes/' . $class . ".php";
     }
+
+
 }
--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -2,16 +2,22 @@
 
 class NewsController
 {
+
     public function actionAll()
     {
-        $items = News::getAll();
-        include __DIR__ . "/../view/news/all.php";
+        $news = News::getAll();
+        $view = new View();
+        $view->assign('items', $news);
+        $view->display("news/all.php");
+
     }
 
     public  function  actionOne()
     {
         $id = $_GET['id'];
-        $item = News::getOne($id);
-        include __DIR__ . "/../view/news/one.php";
-    }
+        $news = News::getOne($id);
+        $view = new View();
+        $view->assign('items', $news);
+        $view->display("news/one.php");
+     }
 }
